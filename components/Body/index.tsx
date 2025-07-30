6bac2ceb3b11d8c65171854e2d70a26d91aec45a
Luiz <nandoluizprimeiro@gmail.com>
2025-07-30 14:35:33 -0300

    Criando estrutura básica do Body

---
+++ components/Body/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+const Body = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.connectedStatus}>
+                <Text style={styles.text}>Bem-vindo ao EduCoChain!</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        paddingHorizontal: 30,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: '#f9f5ebff',
+    },
+    connectedStatus: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#5d1923',
+        fontSize: 17,
+        fontWeight: 'bold',
+    },
+});
+
+export default Body;
