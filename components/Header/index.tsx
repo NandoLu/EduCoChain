afcc9d621c01dc7d7896b324402eba48617a16dd
Luiz <nandoluizprimeiro@gmail.com>
2025-07-30 14:10:02 -0300

    Adicionando greenDot e ajustando estilização

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,37 +1,51 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const Header = ({ }) => {
+const Header = () => {
     return (
         <View style={styles.container}>
-
             <Image
                 source={require('../../assets/images/EduCoChain-logo.png')}
                 style={styles.logo}
             />
-
-            <Text style={styles.text}>Tela Home</Text>
-
+            <View style={styles.connectedStatus}>
+                <Text style={styles.text}>Conectado</Text>
+                <View style={styles.greenDot} />
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
+        paddingTop: 40,
         padding: 20,
         width: '100%',
-        height: '13%',
+        height: '15%',
         alignItems: 'center',
-        backgroundColor: '#f3ebd2ff',
-        gap: 20,
+        paddingHorizontal: 30,
         flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: '#f2eee2', 
     },
     logo: {
         height: 50,
         width: 100,
+        resizeMode: 'contain',
+    },
+    connectedStatus: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    greenDot: {
+        width: 10,  // Tamanho da bolinha
+        height: 10,
+        borderRadius: 5, 
+        backgroundColor: '#28a745', 
+        marginLeft: 10, 
     },
     text: {
-        color: '#5d1923', // Cor do texto padrão
+        color: '#5d1923',
         fontSize: 16,
         fontWeight: 'bold',
     },
