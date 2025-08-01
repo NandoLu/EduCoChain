abe55f16be70a84fc3f35080091ae24065ca339f
Luiz <nandoluizprimeiro@gmail.com>
2025-08-01 10:35:43 -0300

    Retirando animações

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }} >
+      <Stack.Screen name="home" options={{ title: 'Início', animation: 'none' }} />
+      <Stack.Screen name="screens/wallet" options={{ title: 'Carteira', animation: 'none' }} />
+      <Stack.Screen name="screens/contracts" options={{ title: 'Contratos', animation: 'none' }} />
+    </Stack>
+  );
 }
