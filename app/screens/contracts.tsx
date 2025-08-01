f5f77be0f06f46e156445b90d16addbcbf701ccc
Luiz <nandoluizprimeiro@gmail.com>
2025-08-01 09:55:48 -0300

    Criando estrutura básica

---
+++ app/screens/contracts.tsx
@@ -0,0 +1,39 @@
+import { Text, View, StyleSheet } from "react-native";
+import Header from '../../components/Header';
+import FooterNav from '../../components/FooterNav';
+import Body from '../../components/Body';
+import { Stack } from "expo-router";
+
+export default function Contracts() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Stack.Screen options={{ title: "Contracts" }} /> 
+      <Body></Body>
+      <FooterNav></FooterNav>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: '#28a745', // Cor verde para sucesso
+  },
+  subtitle: {
+    fontSize: 20,
+    marginBottom: 20,
+    color: '#333',
+  },
+  header:{
+    width: '100%',
+    borderWidth:1,
+  }
+});
